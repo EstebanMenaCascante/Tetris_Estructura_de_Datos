0510da0a9163024b931b31857db7913e7fa8ab58
Esteban Josue Mena Cascante <esteban.mena.cascante@est.una.ac.cr>
2026-09-23 22:36:42 -0600

    Game over

--- Tetris_Estructura_de_Datos/main.cpp
+++ Tetris_Estructura_de_Datos/main.cpp
@@ -26,126 +26,149 @@ int main(int argc, char *argv[])
 
 	float tiempoCaida = 0.0f;
 	float velocidadCaida = 0.5f;
-	
+
 	float tiempoMovLateral = 0.0f;
 	float retardoMovimiento = 0.12f;
+
+	bool gameOver = false;
 
 	while (!WindowShouldClose())
 	{
 
 		float deltaTime = GetFrameTime();
-		tiempoCaida += deltaTime;
-		if (tiempoCaida >= velocidadCaida)
+		if (!gameOver)
 		{
-			tiempoCaida = 0.0f;
-
-			if (!moverPieza(piezaActual, 0, 1, tablero))
+			tiempoCaida += deltaTime;
+			if (tiempoCaida >= velocidadCaida)
 			{
-				for (int bloque = 0; bloque < 4; bloque++)
+				tiempoCaida = 0.0f;
+				if (!moverPieza(piezaActual, 0, 1, tablero))
 				{
-					int col = obtenerXBloque(piezaActual, bloque);
-					int fila = obtenerYBloque(piezaActual, bloque);
+					for (int bloque = 0; bloque < 4; bloque++)
+					{
+						int col = obtenerXBloque(piezaActual, bloque);
+						int fila = obtenerYBloque(piezaActual, bloque);
+						tablero.colocarCelda(fila, col, (int)piezaActual.tipo + 1);
+					}
+					tablero.limpiarFilas();
+					piezaActual = crearPieza(cola.desencolar());
+					cola.rellenarSiEsNecesario();
 
-					tablero.colocarCelda(fila, col, (int)piezaActual.tipo + 1);
+					if (!posicionValida(piezaActual, tablero))
+					{
+						gameOver = true;
+					}
+					hold.desbloquear();
 				}
-				tablero.limpiarFilas();
-				piezaActual = crearPieza(cola.desencolar());
-				cola.rellenarSiEsNecesario();
-
-				hold.desbloquear();
 			}
-		}
 
-		if (IsKeyPressed(KEY_LEFT)) {
-			moverPieza(piezaActual, -1, 0, tablero);
-			tiempoMovLateral = 0.0f;
-		} 
-		else if (IsKeyDown(KEY_LEFT)) {
-			tiempoMovLateral += deltaTime;
-			if (tiempoMovLateral >= retardoMovimiento) {
+			if (IsKeyPressed(KEY_LEFT))
+			{
 				moverPieza(piezaActual, -1, 0, tablero);
 				tiempoMovLateral = 0.0f;
 			}
-		}
-		if (IsKeyPressed(KEY_RIGHT)) {
-			moverPieza(piezaActual, 1, 0, tablero);
-			tiempoMovLateral = 0.0f;
-		}
-		else if (IsKeyDown(KEY_RIGHT)) {
-			tiempoMovLateral += deltaTime;
-			if (tiempoMovLateral >= retardoMovimiento) {
+			else if (IsKeyDown(KEY_LEFT))
+			{
+				tiempoMovLateral += deltaTime;
+				if (tiempoMovLateral >= retardoMovimiento)
+				{
+					moverPieza(piezaActual, -1, 0, tablero);
+					tiempoMovLateral = 0.0f;
+				}
+			}
+			if (IsKeyPressed(KEY_RIGHT))
+			{
 				moverPieza(piezaActual, 1, 0, tablero);
 				tiempoMovLateral = 0.0f;
 			}
-		}
-		if (IsKeyDown(KEY_DOWN)) {
-			velocidadCaida = 0.05f;
-		} else {
-			velocidadCaida = 0.5f;
-		}
-
-		if (IsKeyPressed(KEY_UP))
-		{
-			rotarPieza(
-				piezaActual,
-				tablero);
-		}
-		
-		if (IsKeyPressed(KEY_C) && hold.puedeIntercambiar())
-		{
-			if (hold.estaVacia())
+			else if (IsKeyDown(KEY_RIGHT))
 			{
-				hold.apilar(piezaActual.tipo);
-				piezaActual = crearPieza(cola.desencolar());
-				cola.rellenarSiEsNecesario();
+				tiempoMovLateral += deltaTime;
+				if (tiempoMovLateral >= retardoMovimiento)
+				{
+					moverPieza(piezaActual, 1, 0, tablero);
+					tiempoMovLateral = 0.0f;
+				}
+			}
+			if (IsKeyDown(KEY_DOWN))
+			{
+				velocidadCaida = 0.05f;
 			}
 			else
 			{
-				TipoPieza guardada = hold.desapilar();
-				hold.apilar(piezaActual.tipo);
-				piezaActual = crearPieza(guardada);
+				velocidadCaida = 0.5f;
 			}
-			hold.bloquear();
+
+			if (IsKeyPressed(KEY_UP))
+			{
+				rotarPieza(
+					piezaActual,
+					tablero);
+			}
+
+			if (IsKeyPressed(KEY_C) && hold.puedeIntercambiar())
+			{
+				if (hold.estaVacia())
+				{
+					hold.apilar(piezaActual.tipo);
+					piezaActual = crearPieza(cola.desencolar());
+					cola.rellenarSiEsNecesario();
+				}
+				else
+				{
+					TipoPieza guardada = hold.desapilar();
+					hold.apilar(piezaActual.tipo);
+					piezaActual = crearPieza(guardada);
+				}
+				hold.bloquear();
+			}
 		}
 
-		BeginDrawing();
-
-		ClearBackground(Color{18, 20, 28, 255});
-
-		DrawText("TETRIS", 60, 60, 50, RAYWHITE);
-
-		DrawText("Estructuras de Datos", 30, 120, 24, GRAY);
-
-		tablero.dibujar(320, 60, 28);
-
-		dibujarPieza(piezaActual, 320, 60, 28);
-
-		DrawText("TABLERO", 650, 100, 28, RAYWHITE);
-
-		DrawText("10 columnas", 650, 150, 20, LIGHTGRAY);
-
-		DrawText("20 filas", 650, 180, 20, LIGHTGRAY);
-
-		DrawText("Lista enlazada", 650, 230, 20, GREEN);
-		
-		DrawText("HOLD (Tecla C)", 50, 200, 20, RAYWHITE);
-		
-		if (!hold.estaVacia())
+		if (!gameOver)
 		{
-			Pieza pHold = crearPieza(hold.verPieza());
-			pHold.x = 0;
-			pHold.y = 0;
-			dibujarPieza(pHold, 50, 240, 28);
+			BeginDrawing();
+
+			ClearBackground(Color{18, 20, 28, 255});
+
+			DrawText("TETRIS", 60, 60, 50, RAYWHITE);
+
+			DrawText("Estructuras de Datos", 30, 120, 24, GRAY);
+
+			tablero.dibujar(320, 60, 28);
+
+			dibujarPieza(piezaActual, 320, 60, 28);
+
+			DrawText("TABLERO", 650, 100, 28, RAYWHITE);
+
+			DrawText("10 columnas", 650, 150, 20, LIGHTGRAY);
+
+			DrawText("20 filas", 650, 180, 20, LIGHTGRAY);
+
+			DrawText("Lista enlazada", 650, 230, 20, GREEN);
+
+			DrawText("HOLD (Tecla C)", 50, 200, 20, RAYWHITE);
+
+			if (!hold.estaVacia())
+			{
+				Pieza pHold = crearPieza(hold.verPieza());
+				pHold.x = 0;
+				pHold.y = 0;
+				dibujarPieza(pHold, 50, 240, 28);
+			}
+
+			DrawText("SIGUIENTES", 650, 300, 20, RAYWHITE);
+
+			for (int i = 0; i < 3; i++)
+			{
+				Pieza pSiguiente = crearPieza(cola.verSiguiente(i));
+				pSiguiente.x = 0;
+				pSiguiente.y = 0;
+				dibujarPieza(pSiguiente, 650, 340 + (i * 90), 28);
+			}
 		}
-		
-		DrawText("SIGUIENTES", 650, 300, 20, RAYWHITE);
-		
-		for (int i = 0; i < 3; i++)
+		else
 		{
-			Pieza pSiguiente = crearPieza(cola.verSiguiente(i));
-			pSiguiente.x = 0; 
-			pSiguiente.y = 0;
-			dibujarPieza(pSiguiente, 650, 340 + (i * 90), 28); 
+			DrawText("GAME OVER", 250, 250, 60, RED);
 		}
 
 		EndDrawing();
